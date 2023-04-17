5364fd50d7acb6909d4acd4314b1e790e962ded0
eejeong <wdwaaa7325@gmail.com>
2023-04-17 15:38:24 +0900

    test

--- src/main/java/com/kbstar/Main.java
+++ src/main/java/com/kbstar/Main.java
@@ -4,11 +4,7 @@ package com.kbstar;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press ⌥⏎ with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press ⌃R or click the green arrow button in the gutter to run the code.
+        int a = 10;
         for (int i = 1; i <= 5; i++) {
 
             // Press ⌃D to start debugging your code. We have set one breakpoint
